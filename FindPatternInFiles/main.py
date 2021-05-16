f2ad974ff5436d1ef0fd2e7c10bc6bd4dcd59f19
Creapermann <creapermann37@gmail.com>
2021-05-16 13:43:52 +0200

    Refactoring of Project 'FindPatternInFiles'

--- FindPatternInFiles/main.py
+++ FindPatternInFiles/main.py
@@ -6,9 +6,7 @@ for folder, subfolders, files in os.walk(path):
     for file in files:
         with open(folder + "\\" + file, "r") as f:
             file_content = f.readlines()
-            pattern = r"\d{3}-\d{3}-\d{4}"
             for text in file_content:
-                match = re.search(pattern, text)
+                match = re.search(r"\d{3}-\d{3}-\d{4}", text)
                 if match is not None:
                     print(match.group())
-            pass
