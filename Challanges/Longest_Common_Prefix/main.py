fbf929fe50a65c122ca8e085d1f45c16474ade8c
Creapermann <creapermann37@gmail.com>
2021-05-19 14:32:41 +0200

    New project added 'Longest_Common_Prefix'

---
+++ Challanges/Longest_Common_Prefix/main.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]):
+        str_lengths = [len(x) for x in strs]
+        str_lengths.sort()
+        shortest_string = str_lengths[0]
+
+        ended = False
+        verified = 0
+
+        for num in range(0, shortest_string):
+            curr_char = strs[0][num]
+            add_up = False
+            for s in strs:
+                if s[num] != curr_char:
+                    add_up = False
+                    ended = True
+                    break
+
+            if ended is not True:
+                verified += 1
+
+            num += 1
+
+        a = "hello"
+        b = a[:2]
+        if verified == 0:
+            return ""
+        return strs[0][:verified]
+
+
+print(Solution.longestCommonPrefix(Solution, ["cir","car"]))
